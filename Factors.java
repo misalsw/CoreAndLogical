4811f89617b6249341ca4b255f5cdea72bbeed37
swap123 <misalswapnil77@gmail.com>
2022-03-07 23:55:45 +0530

    Core

---
+++ Factors.java
@@ -0,0 +1,31 @@
+//package com.core;
+//
+//import java.util.Scanner;
+//
+//public class Factors {
+//
+//    public static void main(String[] args) {
+//       // Scanner scan = new Scanner(System.in);
+//
+//        public boolean prime(int n) {
+//        for (int i = 2; i <= n; i++ ) {
+//                if (n % i == 0)
+//                    return false;
+//        }
+//        return true;
+//    }
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("Please Enter the Number to check out: ");
+//        int n = scan.nextInt();
+//
+//        for (int i = 1; i * i <= n; i++) {
+//                if (n % i == 0) {
+//                    if (prime(i)) {
+//                        System.out.println(i);
+//                    }
+//                }
+//        }
+//        scan.close();
+//
+//    }
+//}
